12cc36498273ae96d24f86f611c925f8fc8465bd
Nicolas Filzi <nfilzi.webservices@gmail.com>
2019-10-30 23:46:40 +0100

    Update population stats seeds data
    Still using staff from Le Wagon, but only friends ❤️

--- population_statistics/support/sql/seeds.sql
+++ population_statistics/support/sql/seeds.sql
@@ -1,91 +1,69 @@
-INSERT INTO 'cities' ('name') VALUES ('Paris');
-INSERT INTO 'cities' ('name') VALUES ('Brussels');
-INSERT INTO 'cities' ('name') VALUES ('Lille');
-INSERT INTO 'cities' ('name') VALUES ('Bordeaux');
-INSERT INTO 'cities' ('name') VALUES ('Beirut');
-INSERT INTO 'cities' ('name') VALUES ('Amsterdam');
-INSERT INTO 'cities' ('name') VALUES ('Marseille');
 INSERT INTO 'cities' ('name') VALUES ('Nantes');
-INSERT INTO 'cities' ('name') VALUES ('Lisbon');
-INSERT INTO 'cities' ('name') VALUES ('Copenhagen');
-INSERT INTO 'cities' ('name') VALUES ('Saõ Paulo');
-INSERT INTO 'cities' ('name') VALUES ('London');
+INSERT INTO 'cities' ('name') VALUES ('Paris');
+INSERT INTO 'cities' ('name') VALUES ('Amsterdam');
+INSERT INTO 'cities' ('name') VALUES ('Tokyo');
+INSERT INTO 'cities' ('name') VALUES ('Shanghai');
 INSERT INTO 'cities' ('name') VALUES ('Barcelona');
 INSERT INTO 'cities' ('name') VALUES ('Lyon');
-INSERT INTO 'cities' ('name') VALUES ('Rio de Janeiro');
-INSERT INTO 'cities' ('name') VALUES ('Shanghai');
+INSERT INTO 'cities' ('name') VALUES ('Copenhagen');
+INSERT INTO 'cities' ('name') VALUES ('Marseille');
+INSERT INTO 'cities' ('name') VALUES ('Lisbon');
 
+-- Nantes ID: 1
 INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Boris', 'Paillard', 30, 1);
+                   VALUES ('Pierre', 'Facq', 35, 1);
 INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Romain', 'Paillard', 31, 1);
+                   VALUES ('Cecile', 'Veneziani', 34, 1);
 INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Sebastien', 'Saunier', 30, 1);
+                   VALUES ('Nicolas', 'Filzi', 28, 1);
 
+-- Paris ID: 2
 INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Anne', 'Collet', 39, 2);
+                   VALUES ('Boris', 'Paillard', 30, 2);
 INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Charlotte', 'Deprez', 27, 2);
+                   VALUES ('Romain', 'Paillard', 31, 2);
+INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
+                   VALUES ('Sebastien', 'Saunier', 30, 2);
+INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
+                   VALUES ('Cedric', 'Menteau', 32, 2);
+INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
+                   VALUES ('Edouard', 'Foussier', 30, 2);
 
+-- Amsterdam: ID 3
 INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Yann', 'Irbah', 30, 3);
+                   VALUES ('Feiko', 'Boerstra', 29, 3);
 INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Francois Xavier', 'Abraham', 30, 3);
+                   VALUES ('Jad', 'Joubran', 25, 3);
 
+-- Tokyo: ID 4
 INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Bertrand', 'Bussac', 30, 4);
+                   VALUES ('Sylvain', 'Pierre', 32, 4);
 INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Cedric', 'Menteau', 30, 4);
+                   VALUES ('Paul', 'Gaumer', 32, 4);
+INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
+                   VALUES ('Doug', 'Berkley', 25, 4);
 
+-- Shanghai: ID 5
 INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Malik', 'El Khoury', 27, 5);
-INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Jad', 'Joubran', 23, 5);
+                   VALUES ('Thibault', 'Genaitay', 28, 5);
 
+-- Barcelona: ID 6
 INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Thomas', 'Wilkinson', 27, 6);
-INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Nicole', 'Saidy', 23, 6);
+                   VALUES ('Ellyn', 'Bouscasse', 27, 6);
 
+-- Lyon: ID 7
 INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Louis', 'Chavane', 29, 7);
+                   VALUES ('Matthieu', 'Cartillier', 32, 7);
 INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Mathieu', 'Bonfils', 29, 7);
+                   VALUES ('Kevin', 'Chavanne', 30, 7);
 
+-- Copenhagen: ID 8
 INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Pierre', 'Facq', 35, 8);
-INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Cecile', 'Veneziani', 31, 8);
-INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Nicolas', 'Filzi', 25, 8);
+                   VALUES ('Nicolas', 'Feer', 29, 8);
 
+-- Marseille: ID 9
 INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Shannon', 'Graybill', 29, 9);
+                   VALUES ('Mathieu', 'Bonfils', 29, 9);
+-- Lisbon: ID 10
 INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Carlos', 'Mendes', 30, 9);
-
-INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Nicolas', 'Feer', 29, 10);
-INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Marina', 'Starkova', 29, 10);
-
-INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Fernando', 'Americano', 35, 11);
-INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Pedro', 'Meyer', 36, 11);
-
-INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Gustavo', 'De Vita', 35, 12);
-INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Ugo', 'Mare', 29, 12);
-
-INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Etienne', 'Moreau', 25, 13);
-INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Mathieu', 'Geiler', 26, 13);
-
-INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Mathieu', 'Le Roux', 36, 14);
-
-INSERT INTO 'inhabitants' ('first_name', 'last_name', 'age', 'city_id')
-                   VALUES ('Thibault', 'Genaitay', 28, 15);
+                   VALUES ('Shannon', 'Graybill', 30, 10);
