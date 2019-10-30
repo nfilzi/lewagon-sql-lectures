9908338940cff6220937f135a7cc441a5e045321
Nicolas Filzi <nfilzi.webservices@gmail.com>
2019-10-30 23:18:30 +0100

    Edit seeds data to be more useful straight with copy pasting lewagon slides example code

--- hospital/support/sql/seeds.sql
+++ hospital/support/sql/seeds.sql
@@ -1,22 +1,21 @@
 INSERT INTO patients ('first_name', 'last_name', 'age') VALUES ('Season', 'Pennel', 47);
 INSERT INTO patients ('first_name', 'last_name', 'age') VALUES ('Fairy', 'Breen', 48);
 INSERT INTO patients ('first_name', 'last_name', 'age') VALUES ('Josefine', 'Dietrich', 61);
-INSERT INTO patients ('first_name', 'last_name', 'age') VALUES ('Jaymie', 'Plummer', 59);
+INSERT INTO patients ('first_name', 'last_name', 'age') VALUES ('Jaymie', 'Plummer', 21);
 INSERT INTO patients ('first_name', 'last_name', 'age') VALUES ('Ezra', 'Rawls', 29);
 INSERT INTO patients ('first_name', 'last_name', 'age') VALUES ('Elly', 'Lafleur', 30);
-INSERT INTO patients ('first_name', 'last_name', 'age') VALUES ('Bette', 'Davenport', 50);
+INSERT INTO patients ('first_name', 'last_name', 'age') VALUES ('Bette', 'Davenport', 21);
 INSERT INTO patients ('first_name', 'last_name', 'age') VALUES ('Zona', 'Boland', 11);
 INSERT INTO patients ('first_name', 'last_name', 'age') VALUES ('Marg', 'Womack', 74);
 INSERT INTO patients ('first_name', 'last_name', 'age') VALUES ('Mellie', 'Thorne', 46);
-
 
 INSERT INTO doctors ('first_name', 'last_name', 'specialty') VALUES ('Gregory', 'House', 'Infectious Disease Specialist');
 INSERT INTO doctors ('first_name', 'last_name', 'specialty') VALUES ('Lisa', 'Cuddy', 'Endocrinologist');
 INSERT INTO doctors ('first_name', 'last_name', 'specialty') VALUES ('James', 'Wilson', 'Oncologist');
 INSERT INTO doctors ('first_name', 'last_name', 'specialty') VALUES ('Eric', 'Foreman', 'Neurologist');
-INSERT INTO doctors ('first_name', 'last_name', 'specialty') VALUES ('Robert', 'Chase', 'Cardiologist');
+INSERT INTO doctors ('first_name', 'last_name', 'specialty') VALUES ('Robert', 'Chase', 'Cardiac Surgery');
 INSERT INTO doctors ('first_name', 'last_name', 'specialty') VALUES ('Allison', 'Cameron', 'Immunologist');
-
+INSERT INTO doctors ('first_name', 'last_name', 'specialty') VALUES ('Thirteen', 'Remy Hadley', 'Infectious Disease Specialist');
 
 INSERT INTO consultations ('doctor_id', 'patient_id', 'date') VALUES (3, 1, '2016-09-25');
 INSERT INTO consultations ('doctor_id', 'patient_id', 'date') VALUES (4, 5, '2016-12-16');
